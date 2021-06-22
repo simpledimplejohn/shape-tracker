3ab974e6ab6ccba2c070b53669818fa9ae0e08c9
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-22 05:07:51 -0700

    add bable and jest

--- src/triangle.js
+++ src/triangle.js
@@ -1,6 +1,6 @@
 // Business Logic
 
-function Triangle(side1, side2, side3) {
+export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
@@ -9,3 +9,5 @@ function Triangle(side1, side2, side3) {
 Triangle.prototype.checkType = function() {
   return "I can't answer that yet!";
 }
+
+
